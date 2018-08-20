aa45e09d2b7e774061f687857ddc24bb684294ac
Roman Himmes <rhimmes@inform-software.com>
2018-08-20 17:24:49 +0200

    Added static pages

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,8 @@
+class StaticPagesController < ApplicationController
+
+  def home
+  end
+
+  def sign_up
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+  get 'static_pages/sign_up'
   resources :commits, param: :sha
+  root 'static_pages#home'
+
+  get '/signup', to: 'static_pages#sign_up' 
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
